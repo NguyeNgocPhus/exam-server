dfe024401fceac7118903e6c49d152d4dc995aa4
Nguyễn Sỹ Tùng <tungaqd@gmail.com>
2020-09-09 10:01:55 +0700

    fix upload files

--- server/api/file/file.controller.js
+++ server/api/file/file.controller.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 async function saveFileP(req, res, next) {
   const { file } = req.files;
-  fs.writeFile(`./upload/programing/${file.name}`, file.data, 'binary', (err) => {
+  fs.writeFile(`${__dirname}/upload/programing/${file.name}`, file.data, 'binary', (err) => {
     if (err) return res.status(400).json({ message: 'Submit thất bại' });
     return res.status(200).json({ message: 'Submit thành công' });
   });
@@ -11,7 +11,7 @@ async function saveFileP(req, res, next) {
 
 async function saveFileD(req, res, next) {
   const { file } = req.files;
-  fs.writeFile(`./upload/design/${file.name}`, file.data, 'binary', (err) => {
+  fs.writeFile(`${__dirname}/upload/design/${file.name}`, file.data, 'binary', (err) => {
     if (err) return res.status(400).json({ message: 'Submit thất bại' });
     return res.status(200).json({ message: 'Submit thành công' });
   });
